0db324705b533361a519cd38e22b27a8e81f1c85
Tomi Javanainen <tomi.javanainen@gmail.com>
2021-09-25 12:33:22 +0300

    Connecting works

--- main.py
+++ main.py
@@ -19,7 +19,22 @@ def main():
     print(token)
     bot = discum.Client(token=token)
 
-    pass
+    @bot.gateway.command
+    def hello_world(response):
+        if response.event.ready_supplemental:
+            user = bot.gateway.session.user
+            print(f'Logged in as {user["username"]}#{user["discriminator"]}')
+
+        if response.event.message:
+            m = response.parsed.auto()
+            guild_id = m['guild_id'] if 'guild_id' in m else None
+            channel_id = m['channel_id']
+            username = m['author']['username']
+            discriminator = m['author']['discriminator']
+            content = m['content']
+            print(f'> guild {guild_id} channel {channel_id} | {username}#{discriminator}: {content}')
+
+    bot.gateway.run(auto_reconnect=True)
 
 
 if __name__ == '__main__':
